547e44eeda9f86de6ad509067a450b4d7fc783b3
natsolyogesh <natsolyogesh@gmail.com>
2025-03-18 11:58:18 +0530

    add one device login

--- models/user.js
+++ models/user.js
@@ -133,6 +133,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     },
+    loggedIn: {
+        type: Boolean,
+        default: false,
+    },
     lastLogin: {
         type: Date,
         default: Date.now,
